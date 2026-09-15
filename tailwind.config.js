c541c3742fd9031e6c1a475e9f65afb11d92fd34
swartrome-web <swartrome@gmail.com>
2026-09-14 19:07:10 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.export = {
+    content: [
+        "./app/**/*.{js, ts, jsx, tsx, mdx}",
+        "./components/**/.{js,ts, jsx,tsx, mdx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
